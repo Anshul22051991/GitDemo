5cc7d68df09259034944d984215c3ade7e532cb0
anshul <anshuljain.jain072@gmail.com>
2024-11-10 22:12:36 -0500

    'changes'

--- src/javaFastPac/Fast.java
+++ src/javaFastPac/Fast.java
@@ -5,8 +5,8 @@ public class Fast {
 	public static void main(String[] args) {
 		System.out.println("Hello");
 		System.out.println("Hello1");
-		System.out.print("Hello2");
-	
+		System.out.print("Hello3");
+	    System.out.println("Hello4");
 	}
 
 }
